7922173220cfb0a66b804a2d183656d960136d37
Georgi Panayotov <smg@smg-bg.net>
2018-11-12 17:31:11 +0200

    Initial commit

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
